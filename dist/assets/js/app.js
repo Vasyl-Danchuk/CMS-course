aa70f386a155cde73ce18dcaff9c52ee5adf13d6
Vasyl Danchuk <danchukvasya@gmail.com>
2021-03-01 18:00:14 +0200

    fixAdapt & fix hover

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -16,16 +16,52 @@ $(document).ready(function () {
         autoplay: true,
         adaptiveHeight: true,
     });
-    let widthScreen = window.screen.width;
+/*    let widthScreen = window.screen.width;
     let offerList = document.getElementById("offerList");
     if (widthScreen <= 576){
         offerList.classList.add("offer_slider");
-    };
+    };*/
 
     $('.offer_slider').slick({
+        slidesToShow: 6,
         arrows: false,
-        dots: true,
-        autoplay: true,
+        dots: false,
+        autoplay: false,
         adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 1188,
+                settings: {
+                    dots: true,
+                    autoplay: true,
+                    centerMode: true,
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    autoplay: true,
+                    dots: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    autoplay: true,
+                    dots: true,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    autoplay: true,
+                    dots: true,
+                    slidesToShow: 1
+                }
+            },
+        ]
     });
 })
